ab1f6fbceef9118f364d04e97618dd54abb9cd0e
ManthanChokshiWHJR <144251412+ManthanChokshiWHJR@users.noreply.github.com>
2024-02-12 20:01:30 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBkgoFtoYnJ34I8UkMZS0Y8-bpb3SV3Ejc",
+    authDomain: "lets-chat-web-app-843f6.firebaseapp.com",
+    databaseURL: "https://lets-chat-web-app-843f6-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-web-app-843f6",
+    storageBucket: "lets-chat-web-app-843f6.appspot.com",
+    messagingSenderId: "171940037396",
+    appId: "1:171940037396:web:4e616359269ca9728df801"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
